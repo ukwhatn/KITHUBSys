040460d01eea7f199a569cff5b4ee2fef353c588
Yuki Watanabe <ukwhatn@gmail.com>
2022-05-24 12:50:45 +0900

    自分のメッセージしかピン留めできないよう修正

--- bot/cogs/PinMessage.py
+++ bot/cogs/PinMessage.py
@@ -19,8 +19,9 @@ class PinMessage(commands.Cog):
             channel = await self.bot.fetch_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
             user = await self.bot.fetch_user(payload.user_id)
-            await message.pin()
-            self.logger.info(f"Pinned: {message.guild.name}/{message.channel.name}/{message.content}, by {user.name}")
+            if user.id == message.author.id:
+                await message.pin()
+                self.logger.info(f"Pinned: {message.guild.name}/{message.channel.name}/{message.content}, by {user.name}")
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
@@ -28,8 +29,9 @@ class PinMessage(commands.Cog):
             channel = await self.bot.fetch_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
             user = await self.bot.fetch_user(payload.user_id)
-            await message.unpin()
-            self.logger.info(f"Unpinned: {message.guild.name}/{message.channel.name}/{message.content}, by {user.name}")
+            if user.id == message.author.id:
+                await message.unpin()
+                self.logger.info(f"Unpinned: {message.guild.name}/{message.channel.name}/{message.content}, by {user.name}")
 
 
 def setup(bot):
