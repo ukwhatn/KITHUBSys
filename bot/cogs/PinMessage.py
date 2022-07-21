f145a0f7e13e99e3b4d713e8b72d610c0f91ec90
Yuki Watanabe <ukwhatn@gmail.com>
2022-07-21 23:29:51 +0900

    ピン留めを許可する条件を変更

--- bot/cogs/PinMessage.py
+++ bot/cogs/PinMessage.py
@@ -19,7 +19,10 @@ class PinMessage(commands.Cog):
             channel = await self.bot.fetch_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
             user = await self.bot.fetch_user(payload.user_id)
-            if user.id == message.author.id:
+            # ピンどめする条件
+            # 1. chのオーナー => 無条件
+            # 2. 管理者権限を持つユーザ => 無条件
+            if user.id == channel.owner_id or channel.permissions_for(await channel.guild.fetch_member(user.id)).administrator:
                 await message.pin()
                 self.logger.info(f"Pinned: {message.guild.name}/{message.channel.name}/{message.content}, by {user.name}")
 
@@ -29,7 +32,7 @@ class PinMessage(commands.Cog):
             channel = await self.bot.fetch_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
             user = await self.bot.fetch_user(payload.user_id)
-            if user.id == message.author.id:
+            if user.id == channel.owner_id or channel.permissions_for(await channel.guild.fetch_member(user.id)).administrator:
                 await message.unpin()
                 self.logger.info(f"Unpinned: {message.guild.name}/{message.channel.name}/{message.content}, by {user.name}")
 
